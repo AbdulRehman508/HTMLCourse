258fa430908949302363dbc017d662712679da0b
Abdul Rehman <77613249+AbdulRehman508@users.noreply.github.com>
2021-12-28 12:45:07 +0500

    parsingArgumentUsingYargs-2

--- File-System/index.js
+++ File-System/index.js
@@ -11,13 +11,61 @@
 
 // 2nd Lec Argument Parsing with Yargs Part I
 
-    const yargs = require("yargs");
+    // const yargs = require("yargs");
+
+    // yargs.command({
+    //     command: 'add',
+    //     describe: 'Add a new note',
+    //     handler: function(){
+    //         console.log('Adding a new notes .. !')
+    //     }
+    // })
+    // yargs.command({
+    //     command: 'remove',
+    //     describe: 'Remove a note',
+    //     handler: function(){
+    //         console.log('Removing a notes .. !')
+    //     }
+    // })
+    // yargs.command({
+    //     command: 'list',
+    //     describe: 'List Your note',
+    //     handler: function(){
+    //         console.log('Listing a notes .. !')
+    //     }
+    // })
+    // yargs.command({
+    //     command: 'read',
+    //     describe: 'Read a note',
+    //     handler: function(){
+    //         console.log('Read a notes .. !')
+    //     }
+    // })
+    // console.log(yargs.argv);
+
+// 3rd Lec Argument Parsing with Yargs Part 2
+
+     const yargs = require("yargs");
 
     yargs.command({
         command: 'add',
         describe: 'Add a new note',
-        handler: function(){
-            console.log('Adding a new notes .. !')
+        builder:{
+            title:{
+                describe:'Note Title ... !',
+                demandOption: true,
+                type:'string',
+            },
+            body:{
+                describe:'Note Body ... !',
+                demandOption: true,
+                type:'string',
+            }
+        },
+        handler: function(argv){
+            console.log("Title:" + argv.title);
+            console.log("Body:" + argv.body);
+            // console.log('Adding a new notes .. !',argv)
         }
     })
     yargs.command({
@@ -41,4 +89,4 @@
             console.log('Read a notes .. !')
         }
     })
-    console.log(yargs.argv);
+    yargs.parse();
