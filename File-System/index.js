1058e0f6b4e6902788191de266b2d3174e007c19
Abdul Rehman <77613249+AbdulRehman508@users.noreply.github.com>
2021-12-28 12:23:01 +0500

    parsingArgumentYargs-1

--- File-System/index.js
+++ File-System/index.js
@@ -1,13 +1,44 @@
 
 // 1st Lec get value from user using command line
 
-console.log(process.argv);
+    // console.log(process.argv);
+    // const command = process.argv[2]
+    // if(command === 'add'){
+    //     console.log('You are added ...!');
+    // }else{
+    //     console.log('Please added some ... !');
+    // }
 
-const command = process.argv[2]
+// 2nd Lec Argument Parsing with Yargs Part I
 
-if(command === 'add'){
-    console.log('You are added ...!');
-}else{
-    console.log('Please added some ... !');
-}
+    const yargs = require("yargs");
 
+    yargs.command({
+        command: 'add',
+        describe: 'Add a new note',
+        handler: function(){
+            console.log('Adding a new notes .. !')
+        }
+    })
+    yargs.command({
+        command: 'remove',
+        describe: 'Remove a note',
+        handler: function(){
+            console.log('Removing a notes .. !')
+        }
+    })
+    yargs.command({
+        command: 'list',
+        describe: 'List Your note',
+        handler: function(){
+            console.log('Listing a notes .. !')
+        }
+    })
+    yargs.command({
+        command: 'read',
+        describe: 'Read a note',
+        handler: function(){
+            console.log('Read a notes .. !')
+        }
+    })
+    console.log(yargs.argv);
