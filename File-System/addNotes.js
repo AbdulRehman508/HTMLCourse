0c1cb8877674daf1bff75b84eb16cebab5af1f0a
Abdul Rehman <77613249+AbdulRehman508@users.noreply.github.com>
2021-12-29 15:40:28 +0500

    listingNotes

--- File-System/addNotes.js
+++ File-System/addNotes.js
@@ -38,40 +38,47 @@
 
 // with Arrow Function
 
-    // const fs = require('fs');
-    // const addNote = (title, body) => {
-    //     const notes = loadNotes()
-    //     const duplicateData = notes.filter(notes => notes.title === title);
-    //     if(duplicateData.length === 0){
-    //         notes.push({
-    //             title:title,
-    //             body:body
-    //         })
-    //         saveNotes(notes);
-    //         console.log("Your note is added ... !");
-    //     }else{
-    //         console.log("Your note is already exist ... !");
-    //     }
+    const fs = require('fs');
+    const addNote = (title, body) => {
+        const notes = loadNotes()
+        const duplicateData = notes.filter(notes => notes.title === title);
+        if(duplicateData.length === 0){
+            notes.push({
+                title:title,
+                body:body
+            })
+            saveNotes(notes);
+            console.log("Your note is added ... !");
+        }else{
+            console.log("Your note is already exist ... !");
+        }
         
-    // }
-    // const removeNotes = (title) => {
-    //    const notes = loadNotes()
-    //    const dataToKeep = notes.filter(notes => notes.title !== title);
-    //    saveNotes(dataToKeep);
-    // }
-    // const saveNotes = (notes) => {
-    //     const dataJson = JSON.stringify(notes);
-    //     fs.writeFileSync('notes.json',dataJson);
-    // }
-    // const loadNotes = () => {
-    //         const dataBuffer = fs.readFileSync('notes.json');
-    //         const dataJSON = dataBuffer.toString();
-    //         return JSON.parse(dataJSON);
-    // }
+    }
+    const removeNotes = (title) => {
+       const notes = loadNotes()
+       const dataToKeep = notes.filter(notes => notes.title !== title);
+       saveNotes(dataToKeep);
+    }
+    const listNotes = () => {
+        const notes = loadNotes()
+        notes.forEach(element => {
+            console.log(element.title + "      "+ element.body);           
+        });
+    }
 
-
-    // module.exports = {
-    //     addNote: addNote,
-    //     removeNotes: removeNotes,
-    //     loadNotes: loadNotes
-    // }
+    const saveNotes = (notes) => {
+        const dataJson = JSON.stringify(notes);
+        fs.writeFileSync('notes.json',dataJson);
+    }
+    const loadNotes = () => {
+            const dataBuffer = fs.readFileSync('notes.json');
+            const dataJSON = dataBuffer.toString();
+            return JSON.parse(dataJSON);
+    }
+  
+    module.exports = {
+        addNote: addNote,
+        removeNotes: removeNotes,
+        loadNotes: loadNotes,
+        listNotes: listNotes
+    }
--- File-System/index.js
+++ File-System/index.js
@@ -119,87 +119,94 @@
 
 // 5th & 6th Lec Adding / removing a Note
 
-    // const notes = require('./addNotes.js')
-    // const yargs = require("yargs");
+    const notes = require('./addNotes.js')
+    const yargs = require("yargs");
 
-    // yargs.command({
-    //     command: 'add',
-    //     describe: 'Add a new note',
-    //     builder:{
-    //         title:{
-    //             describe:'Note Title ... !',
-    //             demandOption: true,
-    //             type:'string',
-    //         },
-    //         body:{
-    //             describe:'Note Body ... !',
-    //             demandOption: true,
-    //             type:'string',
-    //         }
-    //     },
-    //     handler: function(argv){
-    //         notes.addNote(argv.title, argv.body);
-    //     }
-    // })
-    //     yargs.command({
-    //     command: 'remove',
-    //     describe: 'Remove a note',
-    //     builder:{
-    //         title:{
-    //             describe:'Note Title ... !',
-    //             demandOption: true,
-    //             type:'string',
-    //         }
-    //     },
-    //     handler: function(argv){
-    //        notes.removeNotes(argv.title);
-    //     }
-    // })
-    //     yargs.command({
-    //     command: 'read',
-    //     describe: 'Read a note',
-    //     handler: function(){
-    //        notes.loadNotes();
-    //     }
-    // })
-    // yargs.parse();
+    yargs.command({
+        command: 'add',
+        describe: 'Add a new note',
+        builder:{
+            title:{
+                describe:'Note Title ... !',
+                demandOption: true,
+                type:'string',
+            },
+            body:{
+                describe:'Note Body ... !',
+                demandOption: true,
+                type:'string',
+            }
+        },
+        handler: function(argv){
+            notes.addNote(argv.title, argv.body);
+        }
+    })
+        yargs.command({
+        command: 'remove',
+        describe: 'Remove a note',
+        builder:{
+            title:{
+                describe:'Note Title ... !',
+                demandOption: true,
+                type:'string',
+            }
+        },
+        handler: function(argv){
+           notes.removeNotes(argv.title);
+        }
+    })
+        yargs.command({
+        command: 'read',
+        describe: 'Read a note',
+        handler: function(){
+           notes.loadNotes();
+        }
+    })
+        yargs.command({
+        command: 'list',
+        describe: 'List Your note',
+        handler(){
+            notes.listNotes();
+        }
+    })
+    yargs.parse();
 
 // 7th Lec Arrow Function
 
-    const square = (x) => x + x;
-    console.log(square(5));
+    // const square = (x) => x + x;
+    // console.log(square(5));
 
-    const event = {
-        name :'Angular Developer',
-        developerList: ['Mamo','Afzal','Hamza','Abdul Rehman'],
-        printName: function(){
-            console.log("Abdul Rehman is a ? ..." + this.name),
-            this.developerList.forEach(element => {
-                console.log(element + " is Web Developer ...!")
-            })
-        }
+    // const event = {
+    //     name :'Angular Developer',
+    //     developerList: ['Mamo','Afzal','Hamza','Abdul Rehman'],
+    //     printName: function(){
+    //         console.log("Abdul Rehman is a ? ..." + this.name),
+    //         this.developerList.forEach(element => {
+    //             console.log(element + " is Web Developer ...!")
+    //         })
+    //     }
 
-    }
-    event.printName()
+    // }
+    // event.printName()
 
-    const tasks = {
-        tasks: [
-            {
-                name : 'Abdul Rehman',
-                complete : true
-            },
-            {
-                name : 'Mamo',
-                complete : false
-            },
-            {
-                name : 'Afzal', 
-                complete : false
-            }
-        ],
-        getIncompleteTask(){
-           return this.tasks.filter(item => item.complete === false)
+    // const tasks = {
+    //     tasks: [
+    //         {
+    //             name : 'Abdul Rehman',
+    //             complete : true
+    //         },
+    //         {
+    //             name : 'Mamo',
+    //             complete : false
+    //         },
+    //         {
+    //             name : 'Afzal', 
+    //             complete : false
+    //         }
+    //     ],
+    //     getIncompleteTask(){
+    //        return this.tasks.filter(item => item.complete === false)
             
-        }
-    }
-    console.log(tasks.getIncompleteTask())
+    //     }
+    // }
+    // console.log(tasks.getIncompleteTask())
