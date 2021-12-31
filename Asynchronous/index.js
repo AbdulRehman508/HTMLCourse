76d205f03aa7f551f141fc1c8cccf7af131a98fb
Abdul Rehman <77613249+AbdulRehman508@users.noreply.github.com>
2021-12-31 12:34:00 +0500

    errorHandling

--- Asynchronous/index.js
+++ Asynchronous/index.js
@@ -21,13 +21,25 @@
 //     //  console.log(data.current);
 //  })
 
- // 5th Http Request geocoding
+// 5th Http Request geocoding
 
- const request = require('request');
- const geocoding = "https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiYWJkdWxyZWhtYW4wMDEiLCJhIjoiY2t4dTFsaThkNXJicjJ1c3Rqem8zb3d6NiJ9.bM1KrM9k5K_U9EkYkXwmuw&limit=1"
+//  const request = require('request');
+//  const geocoding = "https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiYWJkdWxyZWhtYW4wMDEiLCJhIjoiY2t4dTFsaThkNXJicjJ1c3Rqem8zb3d6NiJ9.bM1KrM9k5K_U9EkYkXwmuw&limit=1"
 
- request({ url: geocoding, json:true},(error,response) => {
-        const latitude = response.body.features[0].center[1];
-        const longitude = response.body.features[0].center[0]; 
-        console.log(latitude,longitude);
- })
+//  request({ url: geocoding, json:true},(error,response) => {
+//         const latitude = response.body.features[0].center[1];
+//         const longitude = response.body.features[0].center[0]; 
+//         console.log(latitude,longitude);
+//  })
+
+// 6th Error Handling
+
+const request = require('request');
+const url = "http://api.weatherstack.com/current?access_key=b25f0dab8921bc25dabf05014d5aa1f3";
+request({url:url, json: true}, (error,response) => {
+       if(!error){
+           console.log("Your request is : " + error)
+       }else{
+           console.log("It is current Temprature  .... " + response.body.current.temperature + " .... Chance of rain " + response.body.current.precip);
+       }
+})
