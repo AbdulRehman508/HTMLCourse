d91580ba58550938bbfabd39334e8f930619e1c6
Abdul Rehman <77613249+AbdulRehman508@users.noreply.github.com>
2022-01-04 11:35:28 +0500

    Serving up HTML and JSON

--- web-server/web-server/src/app.js
+++ web-server/web-server/src/app.js
@@ -10,7 +10,17 @@ app.get('/help',(req,res)=>{
     res.send('<h1>What are the help of serve ..!</h1>')
 })
 app.get('/about',(req,res)=>{
-    res.send('<h1>Most popular server!</h1>')
+    res.send([
+        {
+            name:'Abdul Rehman',
+            contact: '+92 313-7313-612'
+        },
+        {
+            name:'Ehtisham-ul-Haq',
+            contact: '+92 343-1790-676'
+        }
+        
+    ])
 })
 app.get('/help/contact',(req,res)=>{
     res.send('<h1>Contact of expressjs serve : +1 02354 786 ..!</h1>')
