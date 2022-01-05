f19cd09d4d8d4afcd53270145ffbad52fd25c716
Abdul Rehman <77613249+AbdulRehman508@users.noreply.github.com>
2022-01-05 13:13:04 +0500

    Wiring up the User Interface

--- web-server/web-server/public/js/app.js
+++ web-server/web-server/public/js/app.js
@@ -1,19 +1,25 @@
-console.log("Wellcome in static file");
 
 
 
-fetch('http://puzzle.mead.io/puzzle').then(res =>{
-    res.json().then( res =>{
-        console.log(res);
-    })
-})
+
+
 
 const formSearch = document.getElementById('searchForm');
 const searchField = document.getElementById('search');
+const locationSearch = document.getElementById('location');
+const puzzle = document.getElementById('puzzle')
 
 formSearch.addEventListener('submit',(e)=>{
     e.preventDefault();
-    const location = searchField.value;
-    console.log('location',location);
-    searchField.value = '';
+        if(searchField.value === ''){
+            locationSearch.innerHTML = "Please must provide a term value ... !"
+        }else{
+            locationSearch.textContent = searchField.value;
+            fetch('http://puzzle.mead.io/puzzle').then(res =>{
+            res.json().then( data =>{
+                puzzle.innerHTML = data.puzzle;
+            })
+            })
+        }
+
 })
