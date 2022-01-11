af2ac071e67d40addb2d11f4959c5cc8ca4d5df9
Abdul Rehman <77613249+AbdulRehman508@users.noreply.github.com>
2022-01-11 10:36:11 +0500

    objectID

--- web-server/task-manager/mongodb.js
+++ web-server/task-manager/mongodb.js
@@ -1,11 +1,20 @@
 
 // CRUD system
 
-const mongodb = require('mongodb');
-const MongoClient = mongodb.MongoClient
+// const mongodb = require('mongodb');
+// const MongoClient = mongodb.MongoClient
+// const ObjectID = mongodb.ObjectID;
+
+//  OR
+
+const { MongoClient, ObjectID } = require('mongodb');
 
 const connectionURL = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager'
+
+const id = new ObjectID();
+console.log(id);
+console.log(id.getTimestamp());
 
 MongoClient.connect(connectionURL,{useNewUrlParser: true}, (error,client)=>{
     if(error){
@@ -15,52 +24,47 @@ MongoClient.connect(connectionURL,{useNewUrlParser: true}, (error,client)=>{
 
         const db = client.db(databaseName)
 
-        // db.collection('users').insertMany(
-        //     [
-        //         {
-        //             name:"Amir Attari",
-        //             address:"Faisalabd",
-        //             age:31
-        //         },
-        //         {
-        //             name:"Furqan",
-        //             address:"Faisalabd",
-        //             age:25
-        //         }
-        //     ],(error,result)=>{
-        //         if(error){
-        //             console.log("Unable to insert document")
-        //         }else{
-        //             console.log(result);
-        //         }
+        db.collection('users').insertOne(
+                {
+                    _id: id,
+                    name:"Furqan Haso",
+                    address:"Faisalabd",
+                    age:25.25
+                }
+            ,(error,result)=>{
+                if(error){
+                    console.log("Unable to insert document")
+                }else{
+                    console.log(result);
+                }
 
-        //     }
-        // )
-
-        db.collection('user-details').insertMany([
-            {
-                name:"Hamza",
-                description:"He is an able man",
-                completed: true
-            },
-            {
-                name:"Adnan",
-                description:"He is an good man",
-                completed: true
-            },
-            {
-                name:"Zain",
-                description:"He is an Well man",
-                completed: false
             }
-        ],(error,result)=>{
-            if(error){
-                console.log("Unable to insert document ... !" )
-            }else{
-                console.log(result)
-            }
-        }
         )
+
+        // db.collection('user-details').insertMany([
+        //     {
+        //         name:"Hamza",
+        //         description:"He is an able man",
+        //         completed: true
+        //     },
+        //     {
+        //         name:"Adnan",
+        //         description:"He is an good man",
+        //         completed: true
+        //     },
+        //     {
+        //         name:"Zain",
+        //         description:"He is an Well man",
+        //         completed: false
+        //     }
+        // ],(error,result)=>{
+        //     if(error){
+        //         console.log("Unable to insert document ... !" )
+        //     }else{
+        //         console.log(result)
+        //     }
+        // }
+        // )
 
     }
 
