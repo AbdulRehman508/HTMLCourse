56c34e02724fe377b6a490202d97d424827c5d2b
Abdul Rehman <77613249+AbdulRehman508@users.noreply.github.com>
2022-01-12 15:19:59 +0500

    Validation-part__1

--- web-server/task-manager/src/db/mongoose.js
+++ web-server/task-manager/src/db/mongoose.js
@@ -1,37 +1,65 @@
 
 const mongoose = require('mongoose')
+const validator = require('validator')
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-API',{
     useNewUrlParser: true,
     useUnifiedTopology: true,
 })
 
-// const User = mongoose.model('User',{
-//     name: { type: String},
-//     address: { type: String},
-//     age: { type: Number},
-// })
-// const me = new User({
-//     name: 'Ehtisham-ul-haq',
-//     address: 'Faisalabad',
-//     age: 24
-// });
-// me.save().then(()=>{
-//     console.log('User-Details: ' + me)
-// }).catch((error)=>{
-//     console.log('Error: ' + error)
-// })
+const User = mongoose.model('User',{
+    name: { 
+        type: String, 
+        required: true,
+        trim: true
+    },
+    address: { 
+        type: String,
+        required: true,
+        trim: true
+    },
+    email:{
+        type: String,
+        required: true,
+        validate(value){
+            if(!validator.isEmail(value)){
+                throw new Error("Email is Invalid ... !");
+            }
+        }
+    },
+    age: { 
+        type: Number,
+        required: true,
+        default: 0,
+        validate(value){
+            if(value < 0){
+                throw new Error("Age must be a positive number ...!")
+            }
+        }
+    },
+})
+const me = new User({
+    name: 'MATLOOB',   //.trim(),     //.toLowerCase(),
+    address: 'Faisalabad',
+    email: 'matloob@gmail.com',
+    age: 02
+});
+me.save().then(()=>{
+    console.log('User-Details: ' + me)
+}).catch((error)=>{
+    console.log('Error: ' + error)
+})
 
-const task = mongoose.model('user-task',{
-        description: { type: String},
-        completed: { type: Boolean},
-    })
-    const me = new task({
-        description: 'Good Product',
-        completed: true,
-    });
-    me.save().then(()=>{
-        console.log('User-Details: ' + me)
-    }).catch((error)=>{
-        console.log('Error: ' + error)
-    })
+// const task = mongoose.model('user-task',{
+//         description: { type: String},
+//         completed: { type: Boolean},
+//     })
+//     const me = new task({
+//         description: 'Good Product',
+//         completed: true,
+//     });
+//     me.save().then(()=>{
+//         console.log('User-Details: ' + me)
+//     }).catch((error)=>{
+//         console.log('Error: ' + error)
+//     })
