318f6bfa82dc8e62ed660e4dadd07faa1aed30c0
Abdul Rehman <77613249+AbdulRehman508@users.noreply.github.com>
2022-01-18 13:56:43 +0500

    Update& Delete End Point

--- web-server/task-manager/src/index.js
+++ web-server/task-manager/src/index.js
@@ -27,16 +27,23 @@ app.post('/users', async (req,res)=>{
     // })
     // res.send('Testing .. !')
 })
-// Goal: CreatingEnd Point
 
-app.post('/task',(req,res)=>{
+// Goal: CreatingEnd Point
+app.post('/tasks', async (req,res)=>{
 
     const task = new taskModel(req.body)
-    task.save().then(()=>{
-        res.status(200).send(task  + "Task is send .. !")
-    }).catch((e)=>{
-       res.status(400).send(e)
-    })
+    try{
+        await task.save();
+        res.status(200).send("Task is created" + task)
+    }catch(e){
+        res.status(400).send(e)
+    }
+
+    // task.save().then(()=>{
+    //     res.status(200).send(task  + "Task is send .. !")
+    // }).catch((e)=>{
+    //    res.status(400).send(e)
+    // })
 })
 
 // ReadEnd Point
@@ -82,7 +89,6 @@ app.get('/getTasks', (req, res) => {
 // Not Working
 app.get('/getTasks/:id', (req, res) => {
     const _id = req.params.id
-
     createUser.findById(_id).then((task) => {
         if (!task) {
             return res.status(404).send()
@@ -93,9 +99,63 @@ app.get('/getTasks/:id', (req, res) => {
     })
 })
 
+// UpdateEnd Point
 
+app.patch('/users/:id', async (req, res) => {
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['name', 'email', 'password', 'age']
+    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
 
+    if (!isValidOperation) {
+        return res.status(400).send({ error: 'Invalid updates!' })
+    }
+    try {
+        const user = await createUser.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
+    
+        if (!user) {
+            return res.status(404).send()
+        }
+        res.send(user)
+    } catch (e) {
+        res.status(400).send(e)
+    }
+})
+// Goal: updateEnd Point
 
+app.patch('/tasks/:id', async (req,res) => {
+
+    const update = Object.keys(req.body);
+    const allowedUpdates = ['description','completed']
+    const isValidOperation = update.every((item) => allowedUpdates.includes(item))
+
+    if(!isValidOperation){
+        return res.status(400).send({ error: 'Invalid updates!' })
+    }
+    try{
+        const task = await taskModel.findByIdAndUpdate(req.params.id, req.body, {new: true, runValidators: true})
+        if (!task) {
+            return res.status(404).send()
+        }
+        res.send(task)
+    } catch (e) {
+        res.status(400).send(e)
+    }
+
+})
+
+// deletingEnd Point
+
+app.delete('/users/:id', async (req,res) => {
+    try{
+        const user = await createUser.findByIdAndDelete(req.params.id);
+        if(!user){
+            res.status(404).send("User Not found ... !")
+        }
+        res.send(user + "User is successfully Deleted ... !")
+    }catch(e){
+        res.status(400).send(e)
+    }
+})
 
 app.listen(port, ()=>{
     console.log('Port is working .. !' + port);
