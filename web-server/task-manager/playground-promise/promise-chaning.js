85a019c68e4c23068cef2e546ef8c992dd6cd252
Abdul Rehman <77613249+AbdulRehman508@users.noreply.github.com>
2022-01-18 12:05:04 +0500

    deleteTaskUsingAsyncAwait

--- web-server/task-manager/playground-promise/promise-chaning.js
+++ web-server/task-manager/playground-promise/promise-chaning.js
@@ -18,3 +18,16 @@ taskModel.findByIdAndDelete('61e10b13d4e809ddd52cfcb6').then((task)=>{
 }).catch((e)=>{
     console.log(e)
 })
+
+
+const deleteTask = async (id) => {
+    await taskModel.findByIdAndDelete(id);
+    const count = await taskModel.countDocuments({completed:false})
+    return count
+}
+deleteTask('61de77965d2ee9306cb30bcd').then((result)=>{
+    console.log(result)
+}).catch((e)=>{
+    console.log(e);
+})
+
