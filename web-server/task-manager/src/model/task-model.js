f627e702f3f758621b33e2b32e5d3a5e79dfaf00
Abdul Rehman <77613249+AbdulRehman508@users.noreply.github.com>
2022-01-26 10:23:33 +0500

    timeStamps

--- web-server/task-manager/src/model/task-model.js
+++ web-server/task-manager/src/model/task-model.js
@@ -1,17 +1,22 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
 
-const taskModel = mongoose.model('user-task',{
-        description: { 
-            type: String,
-            trim: true,
-            required: true
-        },
-        completed: { 
-            type: Boolean,
-            default: false
-        },
-    })
+const userSchema = new mongoose.Schema({
+    description: { 
+        type: String,
+        trim: true,
+        required: true
+    },
+    completed: { 
+        type: Boolean,
+        default: false
+    },
+},{
+    timestamps:true
+})
+
+
+const taskModel = mongoose.model('user-task',userSchema)
 
     // const me = new task({
     //     description: 'Nice Quality',
