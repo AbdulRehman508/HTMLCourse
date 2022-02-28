a519eb7098fd36f9305e5e3ec011f57cef8a4977
Abdul Rehman <77613249+AbdulRehman508@users.noreply.github.com>
2022-02-28 12:44:09 +0500

    connect socket.io

--- web-server/chat-app/src/index.js
+++ web-server/chat-app/src/index.js
@@ -1,15 +1,22 @@
 
-const path = require("path");
 const express = require("express");
-
 const app = express();
-const port = process.env.port || 3000
+const path = require("path");
+const http = require("http")
+const server = http.createServer(app);
+const { Server} = require("socket.io")
+const io = new Server(server);
+
+const port = process.env.PORT || 3000;
 const pathDirectory = path.join(__dirname, '../public')
-app.use(express.static(pathDirectory));
 
+app.use(express.static(pathDirectory)); 
 
+io.on('connection', ()=>{
+    console.log("New Web socket connection")
+})
 
-app.listen(port,()=>{
+server.listen(port,()=>{
     console.log("Port is working ... !" + port);
 })
 
