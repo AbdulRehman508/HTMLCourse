b0c174f5bb760159a5d103266a41b5529ca0ae3f
Abdul Rehman <77613249+AbdulRehman508@users.noreply.github.com>
2022-02-28 15:11:18 +0500

    first-chat-box

--- web-server/chat-app/public/js/chat.js
+++ web-server/chat-app/public/js/chat.js
@@ -1,3 +1,25 @@
 // const { isObject } = require("util");
 
-var socket = io();
+const socket = io();
+
+// socket.on('Count is Update',(count)=>{
+//     console.log("The Count is Updated ... !"+ count)
+// })
+
+// const increment = document.getElementById("increment");
+// increment.addEventListener('click',()=>{
+//     console.log("Checked")
+//     socket.emit("increment")
+// })
+
+socket.on('message',(message)=>{
+    console.log(message)
+})
+const formSubmit = document.getElementById("form-submit");
+
+formSubmit.addEventListener('submit', (e)=>{
+    e.preventDefault();
+    const message = document.getElementById("message")
+    socket.emit("sendMessage",message.value)
+    message.value = '';
+})
--- web-server/chat-app/src/index.js
+++ web-server/chat-app/src/index.js
@@ -12,8 +12,19 @@ const pathDirectory = path.join(__dirname, '../public')
 
 app.use(express.static(pathDirectory)); 
 
-io.on('connection', ()=>{
+// let count = 0
+io.on('connection', (socket)=>{
     console.log("New Web socket connection")
+
+    socket.emit("message","Wellcome ... !")
+    // socket.emit('Count is Update',count)
+    // socket.on("increment", ()=>{
+    //     count ++;
+    //     io.emit('Count is Update',count)
+    // })
+    socket.on("sendMessage",(message)=>{
+        io.emit("message",message)
+    })
 })
 
 server.listen(port,()=>{
